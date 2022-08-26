71f88a4edb7ccbe6bc128b98bfc4cb6e46b77ca7
P0001842 <P0001842@NB0A165.DBS.DOM>
2022-08-26 15:45:49 +0900

    springBoot 버전 2 페이징 처리 및 serviceImpl 미사용 버전

---
+++ src/main/java/com/study/domain/post/PostService.java
@@ -0,0 +1,76 @@
+package com.study.domain.post;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.study.common.dto.SearchDto;
+import com.study.paging.Pagination;
+import com.study.paging.PagingResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostMapper postMapper;
+
+    /**
+     * 게시글 저장
+     * @param params - 게시글 정보
+     * @return Generated PK
+     */
+    @Transactional
+    public Long savePost(final PostRequest params) {
+        postMapper.save(params);
+        return params.getId();
+    }
+
+    /**
+     * 게시글 상세정보 조회
+     * @param id - PK
+     * @return 게시글 상세정보
+     */
+    public PostResponse findPostById(final Long id) {
+        return postMapper.findById(id);
+    }
+
+    /**
+     * 게시글 수정
+     * @param params - 게시글 정보
+     * @return PK
+     */
+    @Transactional
+    public Long updatePost(final PostRequest params) {
+        postMapper.update(params);
+        return params.getId();
+    }
+
+    /**
+     * 게시글 삭제
+     * @param id - PK
+     * @return PK
+     */
+    public Long deletePost(final Long id) {
+        postMapper.deleteById(id);
+        return id;
+    }
+
+    /**
+     * 게시글 리스트 조회
+     * @param params - search conditions
+     * @return list & pagination information
+     */
+    public PagingResponse<PostResponse> findAllPost(final SearchDto params) {
+        int count = postMapper.count(params);
+        Pagination pagination = new Pagination(count, params);
+        params.setPagination(pagination);
+
+        List<PostResponse> list = postMapper.findAll(params);
+        return new PagingResponse<>(list, pagination);
+    }
+
+}
